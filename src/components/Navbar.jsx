fda9100aa7d4455be2f7f94ffe2ff2f649ac00b5
Rehan Raza <rehanrazasayed@gmail.com>
2025-03-06 12:01:52 +0530

    navbar css revinded to tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,28 +3,28 @@ import { Link } from 'react-router-dom'; // Import Link for routing
 const Navbar = () => {
   return (
     <div className="w-3/5 mx-auto pt-[8vh] pb-[8vh] text-center"> {/* Tailwind for navbar wrapper */}
-      <nav>
+      <nav className="flex justify-center space-x-4">
         <Link 
           to="/" 
-          className="text-black text-[20px] px-4 py-3 font-montserrat transition duration-200 ease-in-out hover:bg-gray-200"
+          className="text-black text-lg px-4 py-2 font-sans transition duration-200 ease-in-out hover:bg-gray-200 rounded-md"
         >
           HOME
         </Link>
         <Link 
           to="/about" 
-          className="text-black text-[20px] px-4 py-3 font-montserrat transition duration-200 ease-in-out hover:bg-gray-200"
+          className="text-black text-lg px-4 py-2 font-sans transition duration-200 ease-in-out hover:bg-gray-200 rounded-md"
         >
           ABOUT
         </Link>
         <Link 
           to="/projects" 
-          className="text-black text-[20px] px-4 py-3 font-montserrat transition duration-200 ease-in-out hover:bg-gray-200"
+          className="text-black text-lg px-4 py-2 font-sans transition duration-200 ease-in-out hover:bg-gray-200 rounded-md"
         >
           PROJECTS
         </Link>
         <Link 
           to="/contact" 
-          className="text-black text-[20px] px-4 py-3 font-montserrat transition duration-200 ease-in-out hover:bg-gray-200"
+          className="text-black text-lg px-4 py-2 font-sans transition duration-200 ease-in-out hover:bg-gray-200 rounded-md"
         >
           CONTACT US
         </Link>
